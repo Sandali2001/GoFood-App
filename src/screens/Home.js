7885b1c9ac118ed58df559a565d74a4afcc766d4
sandali jaiswal <sandalijaiswal50@gmail.com>
2023-11-14 00:10:35 +0530

    second change

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -2,13 +2,19 @@ import React from "react";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 import Cards from "../components/Cards";
+import Carousel from "../components/Carousel";
 
 export default function Home() {
   return (
     <div>
       <Navbar />
-      <Cards />
-      <Footer />
+      <Carousel />
+      <div className="m-3">
+        <Cards />
+        <Cards />
+        <Cards />
+        <Footer />
+      </div>
     </div>
   );
 }
